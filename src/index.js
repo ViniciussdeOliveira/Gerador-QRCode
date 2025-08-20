2392786885caf89f1571128c09427ac03bb4fdc1
Vinicius de Oliveira Santos <githubdovinicius@gmail.com>
2025-08-20 19:55:30 -0300

    Feat: Adicionando servico de geracao de qrcode e senhas

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import prompt from "prompt"
+import promptSchemaMain from "./prompts-schema/schema-main.js"
+import createQRCode from "./services/qrCode/create.js"
+import createPassword from "./services/password/create.js";
+
+async function main(){
+    prompt.get(promptSchemaMain, async (err, result) => {
+        if(err) console.log(err);
+        
+        if(result.select == 1) await createQRCode();
+        if(result.select == 2) await createPassword();
+    });
+
+    prompt.start();
+}
+
+main()
---
+++ src/services/qrCode/create.js
@@ -0,0 +1,9 @@
+import prompt from "prompt";
+import promptSchemaQRCode from "../../prompts-schema/schema-qrcode.js";
+import handle from "./handle.js";
+
+async function createQRCode(){
+    prompt.get(promptSchemaQRCode, handle)
+}
+
+export default createQRCode;
